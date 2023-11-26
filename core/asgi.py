0f6880b53181e0062ea44c8a2a0ff6f8e84052ad
hazem hamdy <hh4morsy@gmail.com>
2023-11-26 21:13:31 +0200

    edit asgi 4

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,7 @@
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter 
 from room.routing import websocket_urlpatterns
