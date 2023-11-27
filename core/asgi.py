ef9bb3a2afd3a5d9b41c6078fa92bac4545f3a22
hazem hamdy <hh4morsy@gmail.com>
2023-11-27 14:26:53 +0200

    change middle ware fore debug

--- core/asgi.py
+++ core/asgi.py
@@ -5,9 +5,19 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter 
 from room.routing import websocket_urlpatterns
+
+from channels.middleware import BaseMiddleware
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DebugMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        logger.debug(f"Received scope: {scope}")
+        return await super().__call__(scope, receive, send)
 application =ProtocolTypeRouter({
     "http":get_asgi_application(),
-    "websocket":AuthMiddlewareStack(
+    "websocket":DebugMiddleware(
         URLRouter(
             websocket_urlpatterns
         )
