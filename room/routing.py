4813c3a1e34002b58ed27575cf59c0e3b5ef2ba4
hazem hamdy <hh4morsy@gmail.com>
2023-11-27 15:39:05 +0200

    edit ws

--- room/routing.py
+++ room/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path , re_path
 from room import consumers
 websocket_urlpatterns = [
-    re_path(r'ws/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/room/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
