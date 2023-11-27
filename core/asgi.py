ba16881b6a79a720e4aa53f89037a68d504865c9
hazem hamdy <hh4morsy@gmail.com>
2023-11-27 15:21:48 +0200

    wss

--- core/asgi.py
+++ core/asgi.py
@@ -5,22 +5,17 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter 
 from room.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 
-from channels.middleware import BaseMiddleware
-import logging
-
-logger = logging.getLogger(__name__)
-
-class DebugMiddleware(BaseMiddleware):
-    async def __call__(self, scope, receive, send):
-        logger.debug(f"Received scope: {scope}")
-        return await super().__call__(scope, receive, send)
 application =ProtocolTypeRouter({
     "http":get_asgi_application(),
-    "websocket":AuthMiddlewareStack(
+    "websocket":
+    AllowedHostsOriginValidator( 
+        AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
-    )
+    ))
+   
 })
 app =application
